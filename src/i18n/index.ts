24d8f5bdbae6c86e9676142c743b73b29b770cda
john.dev <johninquest@gmail.com>
2026-05-31 04:13:33 +0200

    fixed translation isssue

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -8,7 +8,7 @@ i18n.use(initReactI18next).init({
     en: { translation: en },
     fr: { translation: fr },
   },
-  lng: localStorage.getItem('language') ?? 'fr',
+  lng: localStorage.getItem('language') ?? 'en',
   fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
